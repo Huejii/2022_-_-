19ff47216046dd45f5188036ee0c48391d470738
Huejii <peach2233@ajou.ac.kr>
2023-01-22 00:07:58 +0900

    230122

---
+++ ch10/potpin.py
@@ -0,0 +1,16 @@
+from Arduino import Arduino
+import time
+
+potPin=0
+ledPin=11
+
+
+board = Arduino("9600", port="COM9")
+board.pinMode(ledPin, "OUTPUT")
+
+
+while True:
+    potValue=board.analogRead(potPin)
+    brightness=potValue/4
+    board.analogWrite(ledPin, brightness)
+    time.sleep(0.01)

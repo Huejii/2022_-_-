7ee2f814c3dcb4e64f5c67ee99098ddd9c065c12
Huejii <peach2233@ajou.ac.kr>
2023-01-22 00:18:54 +0900

    Update potpin.py

--- ch10/potpin.py
+++ ch10/potpin.py
@@ -5,7 +5,7 @@ potPin=0
 ledPin=11
 
 
-board = Arduino("9600", port="COM9")
+board = Arduino("9600", port="COM7") #포트변경
 board.pinMode(ledPin, "OUTPUT")
 
 
@@ -13,4 +13,4 @@ while True:
     potValue=board.analogRead(potPin)
     brightness=potValue/4
     board.analogWrite(ledPin, brightness)
-    time.sleep(0.01)
+    time.sleep(0.1)

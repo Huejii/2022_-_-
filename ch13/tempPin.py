9023f127ed871fc6e4c130206e698c7b787d2432
Huejii <peach2233@ajou.ac.kr>
2023-01-27 14:38:20 +0900

    Update tempPin.py

--- ch13/tempPin.py
+++ ch13/tempPin.py
@@ -9,6 +9,7 @@ while True:
     value = board.analogRead(tempPin)
     print("value: ", value)
 
+    # 섭씨온도 = ADC(analogRead로 읽은 값) / 1024 * 전압(mv)/10(mv/C)
     millivolts = (value / 1024.0) * 5000.0
     celsius = (millivolts)/ 10.0
 
